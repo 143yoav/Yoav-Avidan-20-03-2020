ca89762ba36cc87469822bdb56004732253d6846
yoav avidan <143yoav@gmail.com>
2020-03-22 23:04:58 +0200

    with build

---
+++ herolo-weather/build/precache-manifest.af65401efaff3f02399d3f8ab138c47e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee36100c5ad3f8f6e32fa24b8994c26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "941e93648b9e73b37549",
+    "url": "/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "5e3539a7189607256c22",
+    "url": "/static/css/main.bbf056f6.chunk.css"
+  },
+  {
+    "revision": "941e93648b9e73b37549",
+    "url": "/static/js/2.00cc86ad.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.00cc86ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e3539a7189607256c22",
+    "url": "/static/js/main.19a8ff11.chunk.js"
+  },
+  {
+    "revision": "b2413390af9fdc7f1aea",
+    "url": "/static/js/runtime-main.8dd17891.js"
+  }
+]);
